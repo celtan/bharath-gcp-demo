9705e3479bd250642a387cae8ae90f78b258d128
Bharath Raj <bharathrajbk@gmail.com>
2019-01-27 10:07:44 +1100

    feat(test): generate data

--- integration-test/test.sh
+++ integration-test/test.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-HOST=35.189.23.120
+HOST=localhost
 
-for i in $(seq 1 1000); do
+for i in $(seq 3 10000); do
 curl -X POST \
-  http://${HOST}:8080/api/ \
+  http://${HOST}:80/api/ \
   -H 'Content-Type: application/json' \
   -H 'cache-control: no-cache' \
-  -d '{
-    "username": "test${i}",
-    "password": "pass${i}"
-}'
+  -d "{
+    \"username\": \"test${i}\",
+    \"password\": \"pass${i}\"
+}"
 done
 # curl -X GET http://localhost:8080/api
 
