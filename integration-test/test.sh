dae411cbcb3b06aee26929391dd709576160b92e
Bharath Raj <bharathraj>
2019-01-26 23:00:24 +0000

    feat(test_data): updated script to generate

--- integration-test/test.sh
+++ integration-test/test.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+HOST=35.189.23.120
+
+for i in $(seq 1 1000); do
 curl -X POST \
-  http://localhost:8080/api/ \
+  http://${HOST}:8080/api/ \
   -H 'Content-Type: application/json' \
   -H 'cache-control: no-cache' \
   -d '{
-    "username": "bharath12",
-    "password": "pass123"
+    "username": "test${i}",
+    "password": "pass${i}"
 }'
+done
+# curl -X GET http://localhost:8080/api
 
-curl -X GET http://localhost:8080/api
-
-curl -X GET http://localhost:8080/api/1
+# curl -X GET http://localhost:8080/api/1
 
